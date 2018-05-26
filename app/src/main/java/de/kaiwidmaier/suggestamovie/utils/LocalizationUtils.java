98596674a0df1249fd3dd6d0abea7385a4fe972d
RealityTest <20795287+RealityTest@users.noreply.github.com>
2018-05-26 10:56:54 +0200

    Added date format setting

--- app/src/main/java/de/kaiwidmaier/suggestamovie/utils/LocalizationUtils.java
+++ app/src/main/java/de/kaiwidmaier/suggestamovie/utils/LocalizationUtils.java
@@ -1,6 +1,10 @@
 package de.kaiwidmaier.suggestamovie.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -22,6 +26,8 @@ public class LocalizationUtils {
   }
 
   public static String getLocalDateFormat(String strDate, Context context) {
+    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+    String format = sharedPref.getString("date_format", "medium");
     SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     Date date = null;
     try {
@@ -29,7 +35,25 @@ public class LocalizationUtils {
     } catch (ParseException e) {
       e.printStackTrace();
     }
-    java.text.DateFormat dateFormat = android.text.format.DateFormat.getMediumDateFormat(context);
+    java.text.DateFormat dateFormat;
+    switch(format){
+      case "short":
+        dateFormat = android.text.format.DateFormat.getDateFormat(context);
+        Log.d("LocalizationUtils", "Short");
+        break;
+      case "medium":
+        dateFormat = android.text.format.DateFormat.getMediumDateFormat(context);
+        Log.d("LocalizationUtils", "Medium");
+        break;
+      case "long":
+        dateFormat = android.text.format.DateFormat.getLongDateFormat(context);
+        Log.d("LocalizationUtils", "Long");
+        break;
+      default:
+        dateFormat = android.text.format.DateFormat.getMediumDateFormat(context);
+        Log.d("LocalizationUtils", "Default");
+        break;
+    }
     return dateFormat.format(date);
   }
 
